34e630d1558c56c8ca0992167015b49ca1cc5ab0
Matteo Cajani <matteo.cajani@pleiade.it>
2018-03-06 15:00:49 +0100

    refactoring build script for ubuntu-elementary-de

--- build-fagia-ubuntu-elementary-de.sh
+++ build-fagia-ubuntu-elementary-de.sh
@@ -2,14 +2,16 @@
 
 set -e
 
+UBUNTU_VERSION="16.04"
 BASE_DIR="$(cd $(dirname $0); pwd)"
 PACKER_DIR="$BASE_DIR/ubuntu/"
-BOX_PATH="$BASE_DIR/builds/ubuntu-16.04.virtualbox.box"
-BOX_JSON="ubuntu-16.04-amd64.json"
-BOX_NAME="fagia/ubuntu-elementary-de-16.04"
+BOX_PATH="$BASE_DIR/builds/ubuntu-$UBUNTU_VERSION.virtualbox.box"
+BOX_JSON="ubuntu-$UBUNTU_VERSION-amd64.json"
+BOX_NAME="fagia/ubuntu-elementary-de-$UBUNTU_VERSION"
 
 rm $BOX_PATH || true
 cd $PACKER_DIR
 packer build -only=virtualbox-iso $BOX_JSON
 vagrant box remove $BOX_NAME || true
 vagrant box add $BOX_NAME $BOX_PATH
+
